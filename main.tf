cd897cdab54b65135127a9e0e6040b0240ab3c86
Eishu Kawaguchi <sikewa-e.k@outlook.jp>
2025-05-21 09:24:52 +0900

    プロバイダ周りの設定

---
+++ main.tf
@@ -0,0 +1,36 @@
+variable "default_profile" {
+  type    = string
+  default = "acu-admin"
+}
+
+variable "default_region" {
+  type    = string
+  default = "ap-northeast-1"
+}
+
+variable "default_tags" {
+  type = map(string)
+
+  default = {
+    Project   = "gimme-scholarship"
+    ManagedBy = "https://github.com/acu4git/gimme-scholarship-terraform"
+  }
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region  = var.default_region
+  profile = var.default_profile
+
+  default_tags {
+    tags = var.default_tags
+  }
+}
