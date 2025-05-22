378d15de1f3e2aff0ab10482dc5e5919cda0c110
Eishu Kawaguchi <sikewa-e.k@outlook.jp>
2025-05-23 04:17:21 +0900

    ルートテーブルの設定

--- vpc.tf
+++ vpc.tf
@@ -70,3 +70,35 @@ resource "aws_internet_gateway" "igw" {
     Name = "main-igw"
   }
 }
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "public-rt"
+    Type = "public"
+  }
+}
+
+resource "aws_route_table_association" "public-rt" {
+  for_each = local.public_subnets
+
+  route_table_id = aws_route_table.public_rt.id
+  subnet_id      = aws_subnet.public_subnets[each.key].id
+}
+
+resource "aws_route_table" "private-rt" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "private-rt"
+    Type = "private"
+  }
+}
+
+resource "aws_route_table_association" "private-rt" {
+  for_each = local.private_subnets
+
+  route_table_id = aws_route_table.private-rt.id
+  subnet_id      = aws_subnet.private_subnets[each.key].id
+}
