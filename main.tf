b2b560934e822a8bdba23b6e0fe72f58d315f2ab
Eishu Kawaguchi <sikewa-e.k@outlook.jp>
2025-05-25 11:52:47 +0900

    リソースの記述を他のファイルに分割

--- main.tf
+++ main.tf
@@ -1,27 +1,3 @@
-variable "project" {
-  type    = string
-  default = "gimme-scholarship"
-}
-
-variable "default_profile" {
-  type    = string
-  default = "acu-admin"
-}
-
-variable "default_region" {
-  type    = string
-  default = "ap-northeast-1"
-}
-
-variable "default_tags" {
-  type = map(string)
-
-  default = {
-    Project   = "gimme-scholarship"
-    ManagedBy = "https://github.com/acu4git/gimme-scholarship-terraform"
-  }
-}
-
 terraform {
   required_providers {
     aws = {
