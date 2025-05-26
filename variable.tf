21ba11e0fd86cc04827facb4648f0e1535539a87
Eishu Kawaguchi <sikewa-e.k@outlook.jp>
2025-05-27 08:28:10 +0900

    変数の管理

---
+++ variable.tf
@@ -0,0 +1,35 @@
+variable "project" {
+  type    = string
+  default = "gimme-scholarship"
+}
+
+variable "default_profile" {
+  type    = string
+  default = "acu-admin"
+}
+
+variable "default_region" {
+  type    = string
+  default = "ap-northeast-1"
+}
+
+variable "default_tags" {
+  type = map(string)
+
+  default = {
+    Project   = "gimme-scholarship"
+    ManagedBy = "https://github.com/acu4git/gimme-scholarship-terraform"
+  }
+}
+
+variable "db_name" {
+  type = string
+}
+
+variable "db_username" {
+  type = string
+}
+
+variable "db_password" {
+  type = string
+}
