575ab1014508a425cc046dce36e0b848c775a42e
Eishu Kawaguchi <sikewa-e.k@outlook.jp>
2025-05-26 10:02:57 +0900

    ECRの用意

---
+++ ecr.tf
@@ -0,0 +1,20 @@
+resource "aws_ecr_repository" "gimme-scholarship" {
+  name = var.project
+  tags = {
+    Name = var.project
+  }
+}
+
+resource "aws_ecr_repository" "gimme-scholarship-task" {
+  name = "${var.project}-task"
+  tags = {
+    Name = "${var.project}-task"
+  }
+}
+
+resource "aws_ecr_repository" "gimme-scholarship-migrate" {
+  name = "${var.project}-migrate"
+  tags = {
+    Name = "${var.project}-migrate"
+  }
+}
