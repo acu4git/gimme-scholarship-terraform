d7d1fb3a948338536c03fc3fc5bbb56ddca1a99f
Eishu Kawaguchi <sikewa-e.k@outlook.jp>
2025-05-28 11:18:48 +0900

    無料利用枠のMySQLを構築

--- rds.tf
+++ rds.tf
@@ -1,13 +1,21 @@
-resource "aws_rds_cluster" "main" {
-  cluster_identifier = "${var.project}-aurora-cluster"
-  engine             = "aurora-mysql"
-  engine_version     = "8.0.mysql_aurora.3.08.2"
+resource "aws_db_instance" "mysql-free-tier" {
+  allocated_storage      = 20
+  engine                 = "mysql"
+  engine_version         = "8.0"
+  instance_class         = "db.t4g.micro"
+  db_name                = var.db_name
+  username               = "admin"
+  password               = random_password.rds_admin_password.result
+  db_subnet_group_name   = aws_db_subnet_group.main.name
+  vpc_security_group_ids = [aws_security_group.db-sg.id]
+  publicly_accessible    = false
+  multi_az               = false
+  storage_type           = "gp2"
 }
 
 resource "aws_db_subnet_group" "main" {
-  name        = "${var.project}-rds-subnet-group"
-  description = "DB subnet group"
-  subnet_ids  = [aws_subnet.private_subnets["rds-1a"].id]
+  name       = "${var.project}-rds-subnet-group"
+  subnet_ids = [aws_subnet.private_subnets["rds-1a"].id, aws_subnet.private_subnets["rds-1c"].id]
   tags = {
     Name = "db-subnet"
   }
@@ -21,9 +29,16 @@ resource "aws_security_group" "db-sg" {
   }
 
   ingress {
-    from_port       = 3306
-    to_port         = 3306
-    protocol        = "tcp"
-    security_groups = []
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
