d9ccf1e8278e21e97771e5c44277aa69602e6447
Eishu Kawaguchi <sikewa-e.k@outlook.jp>
2025-05-31 12:13:36 +0900

    alb to ecs

--- alb.tf
+++ alb.tf
@@ -6,44 +6,56 @@ resource "aws_lb" "alb" {
   subnets            = [aws_subnet.public_subnets["backend-1a"].id, aws_subnet.public_subnets["backend-1c"].id]
 }
 
-# resource "aws_lb_listener" "alb_listener" {
-#   load_balancer_arn = aws_lb.alb.arn
-#   port              = 443
-#   protocol          = "HTTPS"
+resource "aws_lb_listener" "http_listener" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = 80
+  protocol          = "HTTP"
 
-#   default_action {
-#     type = "fixed-response"
+  default_action {
+    type = "redirect"
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
 
-#     fixed_response {
-#       content_type = "text/plain"
-#       message_body = "Not found"
-#       status_code  = "404"
-#     }
-#   }
-# }
+resource "aws_lb_listener" "https_listener" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = 443
+  protocol          = "HTTPS"
+  certificate_arn   = "arn:aws:acm:${var.default_region}:${var.account_id}:certificate/dd015d1e-d3f3-4e5f-a021-dc22a1f148e4"
 
-# resource "aws_lb_listener_rule" "alb_listener_rule" {
-#   listener_arn = aws_lb_listener.alb_listener.arn
-#   priority     = 100
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.api_tg.arn
+  }
+}
 
-#   action {
-#     type             = "forward"
-#     target_group_arn = aws_lb_target_group.alb_target_group.arn
-#   }
+resource "aws_lb_target_group" "api_tg" {
+  name        = "api-tg"
+  protocol    = "HTTP"
+  target_type = "ip"
+  port        = 8080
+  vpc_id      = aws_vpc.main.id
 
-#   condition {
-#     path_pattern {
-#       values = ["/"]
-#     }
-#   }
-# }
+  health_check {
+    enabled             = true
+    path                = "/health"
+    protocol            = "HTTP"
+    port                = "traffic-port"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 3
+    unhealthy_threshold = 3
+    matcher             = "200"
+  }
 
-# resource "aws_lb_target_group" "alb_target_group" {
-#   name        = "alb-target-group"
-#   target_type = "ip"
-#   port        = 80
-#   vpc_id      = aws_vpc.main.id
-# }
+  tags = {
+    Name = "${var.project}-api-tg"
+  }
+}
 
 resource "aws_security_group" "sg-alb" {
   name   = "alb-sg"
@@ -64,9 +76,9 @@ resource "aws_security_group" "sg-alb" {
   }
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port       = aws_lb_target_group.api_tg.port
+    to_port         = aws_lb_target_group.api_tg.port
+    protocol        = "tcp"
+    security_groups = [aws_security_group.webapi-sg.id]
   }
 }
