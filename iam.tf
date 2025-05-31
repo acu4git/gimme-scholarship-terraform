15b6115bd8189b4aa8d537dcb34d1f532d8d1132
Eishu Kawaguchi <sikewa-e.k@outlook.jp>
2025-05-31 12:14:52 +0900

    ecs role

--- iam.tf
+++ iam.tf
@@ -1,3 +1,4 @@
+### for bastion(EC2)
 data "aws_iam_policy" "ssmManagedPolicy" {
   arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
@@ -23,4 +24,48 @@ resource "aws_iam_role" "bastion" {
 resource "aws_iam_role_policy_attachment" "bastion" {
   role       = aws_iam_role.bastion.name
   policy_arn = data.aws_iam_policy.ssmManagedPolicy.arn
+}
+
+### for ECS
+# ECS Task Execution Role
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecs_task_execution_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+        Action = ["sts:AssumeRole"]
+        Effect = "Allow"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_basic_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_iam_role_policy" "ecs_secretsmanager_access" {
+  name = "ecs-secretsmanager-access"
+  role = aws_iam_role.ecs_task_execution_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "secretsmanager:GetSecretValue",
+          "secretsmanager:DescribeSecret",
+          "kms:Decrypt"
+        ]
+        Resource = ["arn:aws:secretsmanager:${var.default_region}:${var.account_id}:secret:rds/gimme-scholarship/password-*"]
+      }
+    ]
+  })
 }
