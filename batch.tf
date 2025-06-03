41826f8f07dea020ffd762f7c51725e647ba0a48
Eishu Kawaguchi <sikewa-e.k@outlook.jp>
2025-06-03 09:50:02 +0900

    奨学金一覧更新バッチ

---
+++ batch.tf
@@ -0,0 +1,21 @@
+resource "aws_cloudwatch_event_rule" "gimme_scholarship_fetch" {
+  name                = "${var.project}-fetch-rule"
+  schedule_expression = "cron(0 1 * * ? *)"
+}
+
+resource "aws_cloudwatch_event_target" "gimme_scholarship_fetch" {
+  rule     = aws_cloudwatch_event_rule.gimme_scholarship_fetch.name
+  role_arn = aws_iam_role.ecs_events_role.arn
+  arn      = aws_ecs_cluster.backend-cluster.arn
+
+  ecs_target {
+    launch_type         = "FARGATE"
+    platform_version    = "LATEST"
+    task_definition_arn = aws_ecs_task_definition.gimme-scholarship-fetch.arn
+    network_configuration {
+      subnets          = [aws_subnet.public_subnets["backend-1a"].id, aws_subnet.public_subnets["backend-1c"].id]
+      security_groups  = [aws_security_group.ecs-db-sg.id]
+      assign_public_ip = true
+    }
+  }
+}
--- iam.tf
+++ iam.tf
@@ -176,3 +176,41 @@ resource "aws_iam_role_policy" "ecr-push" {
     ]
   })
 }
+
+### EventBridge
+resource "aws_iam_role" "ecs_events_role" {
+  name = "ecs-events-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "events.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "ecs_events_policy" {
+  name = "ecs-events-policy"
+  role = aws_iam_role.ecs_events_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "ecs:RunTask",
+          "ecs:DescribeTasks",
+          "iam:PassRole"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
